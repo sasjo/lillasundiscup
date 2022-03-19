5abbaddc5c4d8d8d9d3f26a335eb4ffcf1233279
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2022-03-20 00:38:32 +0100

    chore: Rotate API key

--- api/src/sheets-api.js
+++ api/src/sheets-api.js
@@ -3,7 +3,7 @@ const { google } = require('googleapis');
 const sheets = google.sheets('v4');
 
 const getSheetValues = async (ranges) => sheets.spreadsheets.values.batchGet({
-  auth: 'AIzaSyBqpHQvETzQ_P3mcqTiq1bhW99hR7xE4nU',
+  auth: process.env.SHEETS_API_KEY,
   spreadsheetId: '16ExfiFqE42bgYDkV7iZk8bcObO-Jn1jGyp2YIeHXgq0',
   ranges,
 }).then((result) => result.data.valueRanges.map(({values}) => values));
