5c396c2ff403d7ef3ab4f317d50c01fb88c77f35
Samuel Sjöberg <samuel.sjoberg@extendaretail.com>
2022-04-25 20:31:03 +0200

    feat: Add map

--- webapp/src/Info.jsx
+++ webapp/src/Info.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
 
 const phoneNumbers = [
   '070-180 02 45',
@@ -62,6 +63,23 @@ const Info = () => {
           <Button variant="text" data-id="1" onClick={showPhoneNumber}>Visa nummer</Button>
         </li>
       </Typography>
+
+      <Typography variant="h5" component="h2" gutterBottom>
+        Vägbeskrivning
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Se karta på <Link href="https://goo.gl/maps/zUkHZuHCanhmr5M16">Google Maps</Link>.
+      </Typography>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1644.4491897130501!2d21.94611021639364!3d65.65931558430174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467f66f89a13b185%3A0x7ace94ac86ede3a2!2sSundis%20konstgr%C3%A4splan!5e0!3m2!1sen!2sse!4v1650911020986!5m2!1sen!2sse"
+        title="Vägbeskrivning Sundis"
+        width="100%"
+        height="450"
+        style={{ border: 0, marginBottom: '10px' }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
     </Container>
   );
 };
