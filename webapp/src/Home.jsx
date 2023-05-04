45454ceefae20a107aab77d9acbd75e942ba2d4f
Pegasuz73 <62516432+Pegasuz73@users.noreply.github.com>
2023-05-04 12:17:56 +0200

    fix: Update Home.jsx (#17)
    Ändrat till 2023

--- webapp/src/Home.jsx
+++ webapp/src/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         <Box>
           <img src={sponsorLogo} alt="Pionjären" style={{ width: '100%', maxHeight: '150px', pointerEvents: 'none', display: 'block', margin: '10px auto' }} />
           <Typography variant="h3" component="h1" align="center">
-            Lilla Sundis Cup 2022
+            Lilla Sundis Cup 2023
           </Typography>
           <Stack
             direction="row"
@@ -38,10 +38,10 @@ const Home = () => {
             spacing={4}
           >
             <Typography variant="body1" align="right">
-              Flickor 14 maj
+              Flickor 13 maj
             </Typography>
             <Typography variant="body1" align="left">
-              Pojkar 15 maj
+              Pojkar 14 maj
             </Typography>
           </Stack>
           <img src={sskLogo} alt="Sunderby SK" style={{ height: '20vmin', maxHeight: '100px', pointerEvents: 'none', display: 'block', margin: '10px auto' }} />
@@ -51,14 +51,14 @@ const Home = () => {
           </Typography>
           <Typography variant="body1" paragraph>
             Pionjären Lilla Sundis Cup är tillbaka. För 24:e gången arrangerar Sunderby SK fotbollscup.
-            Efter två Coronapräglade säsonger med inställda fotbollsevent är vi mer taggade än någonsin.
+           
           </Typography>
           <Typography variant="h5" component="h2">
             Deltagande lag och spelschema
           </Typography>
           <Typography variant="body1" paragraph>
-            I år deltar flick- och pojklag där spelarna är födda 2010. Nyfiken på vilka lag som spelar?
-            Se fliken lag. Cupen spelas lördag 14 maj samt söndag 15 maj på Sundis konstgräsplan.
+            I år deltar flick- och pojklag där spelarna är födda 2011. Nyfiken på vilka lag som spelar?
+            Se fliken lag. Cupen spelas lördag 13 maj samt söndag 14 maj på Sundis konstgräsplan.
             Spelschema finner du under fliken matcher.
           </Typography>
         </Box>
